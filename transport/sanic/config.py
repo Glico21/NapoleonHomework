f800538e50606ac5f0f2cebf99fb4f3715c96e80
Dmitriy Boyko <treidman@mail.ru>
2021-01-03 22:56:09 +0500

    create sanic framework config file

---
+++ transport/sanic/config.py
@@ -0,0 +1,8 @@
+import os
+
+
+class SanicConfig:
+    host = os.getenv('host', 'localhost')
+    port = int(os.getenv('port', 8000))
+    workers = int(os.getenv('workers', '1'))
+    debug = bool(os.getenv('debug', False))

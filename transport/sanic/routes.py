886763629abe93974f46c5290af1d7572e1abd75
Dmitriy Boyko <treidman@mail.ru>
2021-01-03 23:12:32 +0500

    create get_routes file

---
+++ transport/sanic/routes.py
@@ -0,0 +1,9 @@
+from typing import Tuple
+
+from configs.config import AppConfig
+from transport.sanic import endpoints
+
+def get_routes(config: AppConfig) -> Tuple:
+    return (
+        #enpoints
+    )

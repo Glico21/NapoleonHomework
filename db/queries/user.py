557325b96ac63ae479337c86ac4be2cbcd08a04c
Dmitriy Boyko <treidman@mail.ru>
2021-01-17 19:16:28 +0500

    update user db queries file

--- db/queries/user.py
+++ db/queries/user.py
@@ -1,16 +1,33 @@
 from api.request import RequestCreateUserDto
 from db.database import DBSession
+from db.exceptions import DBUserExistsException, DBUserNotExistException
 from db.models import DBUser
 
 
-def create_user(session: DBSession, user: RequestCreateUserDto) -> DBUser:
+def create_user(session: DBSession, user: RequestCreateUserDto, hashed_password: bytes) -> DBUser:
     new_user = DBUser(
         login=user.login,
-        password=user.password,
+        password=hashed_password,
         first_name=user.first_name,
         last_name=user.last_name
     )
 
+    if session.get_user_by_login(new_user.login) is not None:
+        raise DBUserExistsException
+
     session.add_model(new_user)
 
     return new_user
+
+
+def get_user(session: DBSession, login: str = None, user_id: int = None) -> DBUser:
+    db_user = None
+
+    if login is not None:
+        db_user = session.get_user_by_login(login)
+    elif user_id is not None:
+        db_user = session.get_user_by_id(user_id)
+
+    if db_user is None:
+        raise DBUserNotExistException
+    return db_user

99c6197bede811060a8e45074c6bee646b7326d6
Dmitriy Boyko <treidman@mail.ru>
2021-01-22 20:42:02 +0500

    update response model for user

--- api/response/user.py
+++ api/response/user.py
@@ -8,9 +8,29 @@ from api.base import ResponseDto
 class ResponseUserDtoSchema(Schema):
     id = fields.Int(required=True)
     login = fields.Str(required=True)
+    created_at = fields.Str(required=True)
+    updated_at = fields.Str(required=True)
     first_name = fields.Str(required=True)
     last_name = fields.Str(required=True)
+
+    @pre_load
+    @post_load
+    def deserialize_datetime(self, data: dict, **kwargs) -> dict:
+        if 'created_at' in data:
+            data['created_at'] = self.datetime_to_iso(data['created_at'])
+        if 'updated_at' in data:
+            data['updated_at'] = self.datetime_to_iso(data['updated_at'])
+
+        return data
+
+    @staticmethod
+    def datetime_to_iso(dt):
+        if isinstance(dt, datetime.datetime):
+            return dt.isoformat()
+        return dt
 
 
 class ResponseUserDto(ResponseDto, ResponseUserDtoSchema):
     __schema__ = ResponseUserDtoSchema
+
+

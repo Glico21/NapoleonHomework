f4cb439a2b09a0b4e6b7cbd39e95b9d8c6b62c43
Dmitriy Boyko <treidman@mail.ru>
2021-01-22 20:43:30 +0500

    add patch and delete queries for user

--- db/queries/user.py
+++ db/queries/user.py
@@ -1,6 +1,8 @@
-from api.request import RequestCreateUserDto
+from typing import List
+
+from api.request import RequestCreateUserDto, RequestPatchUserDto
 from db.database import DBSession
-from db.exceptions import DBUserExistsException, DBUserNotExistException
+from db.exceptions import DBUserExistsException, DBUserNotExistsException
 from db.models import DBUser
 
 
@@ -20,7 +22,7 @@ def create_user(session: DBSession, user: RequestCreateUserDto, hashed_password:
     return new_user
 
 
-def get_user(session: DBSession, login: str = None, user_id: int = None) -> DBUser:
+def get_user(session: DBSession, *, login: str = None, user_id: int = None) -> DBUser:
     db_user = None
 
     if login is not None:
@@ -29,5 +31,26 @@ def get_user(session: DBSession, login: str = None, user_id: int = None) -> DBUs
         db_user = session.get_user_by_id(user_id)
 
     if db_user is None:
-        raise DBUserNotExistException
+        raise DBUserNotExistsException
     return db_user
+
+
+def patch_user(session: DBSession, user: RequestPatchUserDto, user_id: int) -> DBUser:
+
+    db_user = session.get_user_by_id(user_id)
+
+    for attr in user.fields:
+        setattr(db_user, attr, getattr(user, attr))
+
+    return db_user
+
+
+def delete_user(session: DBSession, user_id: int) -> DBUser:
+
+    db_user = session.get_user_by_id(user_id)
+    db_user.is_deleted = True
+    return db_user
+
+
+def get_users(session: DBSession) -> List['DBUser']:
+    return session.get_user_all()

1ea5fca919ebfbf6bff88994899683f49555d9b8
Dmitriy Boyko <treidman@mail.ru>
2021-01-22 19:04:44 +0500

    updated create user request file

--- api/request/create_user.py
+++ api/request/create_user.py
@@ -1,4 +1,4 @@
-from marshmallow import Schema, fields, post_load
+from marshmallow import Schema, fields
 
 from api.base import RequestDto
 
@@ -8,11 +8,6 @@ class RequestCreateUserDtoSchema(Schema):
     password = fields.Str(required=True, allow_none=False)
     first_name = fields.Str(required=True, allow_none=False)
     last_name = fields.Str(required=True, allow_none=False)
-
-    # @post_load
-    # def hash_password(self, data:dict, **kwargs) -> dict:
-    #     data['password'] = generate_hash(data['password'])
-    #     return data
 
 
 class RequestCreateUserDto(RequestDto, RequestCreateUserDtoSchema):

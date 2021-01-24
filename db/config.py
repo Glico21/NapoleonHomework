37c578bf2dec97620ddfb68412d44eb7f4d9f379
Dmitriy Boyko <treidman@mail.ru>
2021-01-24 20:03:07 +0500

    fix db names

--- db/config.py
+++ db/config.py
@@ -10,9 +10,9 @@ class SQLiteConfig:
 
 
 class PostgresConfig:
-    name = os.getenv('POSTGRES_NAME', 'NapoleonHW')
+    name = os.getenv('POSTGRES_NAME', 'NapoleonHomework')
     user = os.getenv('POSTGRES_USER', 'admin')
     password = os.getenv('POSTGRES_PASSWORD', 'qwerty')
-    host = os.getenv('POSTGRES_HOST', 'NapoleonHW-db')
+    host = os.getenv('POSTGRES_HOST', 'NapoleonHomework-db')
     port = os.getenv('POSTGRES_PORT', '5432')
     url = rf'postgresql+psycopg2://{user}:{password}@{host}:{port}/{name}'

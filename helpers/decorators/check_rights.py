baad41cbe50c4b05704ecb88259e3d340bf15a71
Dmitriy Boyko <treidman@mail.ru>
2021-01-27 23:32:14 +0500

    update check id right access endpoint

--- helpers/decorators/check_rights.py
+++ helpers/decorators/check_rights.py
@@ -1,7 +1,9 @@
-
 def check_id_rights_access_decorator(method):
-    async def check_id_rights_access(self, request, body, session, uid, token, *args, **kwargs):
-        if token.get('uid') != uid:
-            return await self.make_response_json(status=403)
-        return await method(self, request, body, session, uid, token, *args, **kwargs)
+    async def check_id_rights_access(self, request, body, *args, **kwargs):
+        if self.auth_required:
+            token = kwargs['token']
+            uid = kwargs.get('uid')
+            if token.get('uid') != uid:
+                return await self.make_response_json(status=403)
+        return await method(self, request, body, *args, **kwargs)
     return check_id_rights_access

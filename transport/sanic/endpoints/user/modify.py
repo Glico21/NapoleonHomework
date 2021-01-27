836c09c41fdfe72eeb4be91061780ba8203f3c44
Dmitriy Boyko <treidman@mail.ru>
2021-01-27 23:49:00 +0500

    update user modify endpoint

--- transport/sanic/endpoints/user/modify.py
+++ transport/sanic/endpoints/user/modify.py
@@ -1,25 +1,22 @@
-from urllib.request import Request
-
+from sanic.request import Request
 from sanic.response import BaseHTTPResponse
 
 from api.request import RequestPatchUserDto
 from api.response import ResponseUserDto
+from transport.sanic.endpoints import BaseEndpoint
+from transport.sanic.exceptions import SanicUserNotFound, SanicDBException
+
 from db.database import DBSession
 from db.exceptions import DBUserNotExistsException, DBDataException, DBIntegrityException
 from db.queries import user as user_queries
-from transport.sanic.endpoints import BaseEndpoint
-from transport.sanic.exceptions import SanicUserNotFound, SanicDBException
 from helpers.decorators import check_id_rights_access_decorator
 
 
 class ModifyUserEndpoint(BaseEndpoint):
     @check_id_rights_access_decorator
     async def method_patch(
-            self, request: Request, body: dict, session: DBSession, uid: int, token: dict, *args, **kwargs
+            self, request: Request, body: dict, session: DBSession, uid: int, *args, **kwargs
     ) -> BaseHTTPResponse:
-
-        if token.get('uid') != uid:
-            return await self.make_response_json(status=403)
 
         request_model = RequestPatchUserDto(body)
 
@@ -37,6 +34,7 @@ class ModifyUserEndpoint(BaseEndpoint):
 
         return await self.make_response_json(status=200, body=response_model.dump())
 
+    @check_id_rights_access_decorator
     async def method_delete(
             self, request: Request, body: dict, session: DBSession, uid: int, *args, **kwargs
     ) -> BaseHTTPResponse:

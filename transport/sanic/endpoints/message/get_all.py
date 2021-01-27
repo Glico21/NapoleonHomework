b6fdcabe710c784aaeff599a9acc2a4a2a5b2b83
Dmitriy Boyko <treidman@mail.ru>
2021-01-27 23:47:01 +0500

    add get all messages endpoint

---
+++ transport/sanic/endpoints/message/get_all.py
@@ -0,0 +1,21 @@
+from sanic.request import Request
+from sanic.response import BaseHTTPResponse
+
+from transport.sanic.endpoints import BaseEndpoint
+from api.response import ResponseMessageDto
+
+from db.database import DBSession
+from db.queries import message as message_queries
+
+
+class GetMessagesEndpoint(BaseEndpoint):
+
+    async def method_get(
+            self, request: Request, body: dict, session: DBSession, token: dict, *args, **kwargs
+    ) -> BaseHTTPResponse:
+
+        uid = token.get('uid')
+        db_message = message_queries.get_messages(session, user_id=uid)
+        response_model = ResponseMessageDto(db_message, many=True)
+
+        return await self.make_response_json(status=200, body=response_model.dump())

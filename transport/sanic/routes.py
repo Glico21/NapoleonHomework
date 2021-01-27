b4e683ad9b6e8dfdb1c6af4fa8354e00cb85fa24
Dmitriy Boyko <treidman@mail.ru>
2021-01-27 23:50:19 +0500

    update routes

--- transport/sanic/routes.py
+++ transport/sanic/routes.py
@@ -14,15 +14,27 @@ def get_routes(config: ApplicationConfig, context: Context) -> Tuple:
             config, context, uri='/user', methods=['POST']
         ),
         endpoints.AuthUserEndpoint(
-            config, context, uri='user/auth', methods=['POST']
+            config, context, uri='/user/auth', methods=['POST']
         ),
         endpoints.ModifyUserEndpoint(
             config, context, uri='/user/<uid:int>', methods=['PATCH', 'DELETE'], auth_required=True
         ),
         endpoints.AllUserEndpoint(
-            config, context, uri='/user/all', methods=['GET'], auth_required=True,
+            config, context, uri='/user/all', methods=['GET']
         ),
         endpoints.OneUserEndpoint(
-            config, context, uri='/user/<uid:int>', methods=['GET'], auth_required=True,
+            config, context, uri='/user/<uid:int>', methods=['GET'], auth_required=True
+        ),
+        endpoints.CreateMessageEndpoint(
+            config, context, uri='/msg', methods=['POST'], auth_required=True
+        ),
+        endpoints.GetMessagesEndpoint(
+            config, context, uri='/msg', methods=['GET'], auth_required=True
+        ),
+        endpoints.ModifyMessageEndpoint(
+            config, context, uri='/msg/<message_id>', methods=['PATCH', 'DELETE'], auth_required=True
+        ),
+        endpoints.OneMessageEndpoint(
+            config, context, uri='/msg/<message_id>', methods=['GET'], auth_required=True
         )
     )

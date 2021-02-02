a269d4a2f5d7316326a2637e60c0c4d82bc312de
Dmitriy Boyko <treidman@mail.ru>
2021-02-02 21:32:32 +0500

    rename decorator

--- helpers/decorators/check_rights.py
+++
@@ -1,9 +0,0 @@
-def check_id_rights_access_decorator(method):
-    async def check_id_rights_access(self, request, body, *args, **kwargs):
-        if self.auth_required:
-            token = kwargs['token']
-            uid = kwargs.get('uid')
-            if token.get('uid') != uid:
-                return await self.make_response_json(status=403)
-        return await method(self, request, body, *args, **kwargs)
-    return check_id_rights_access

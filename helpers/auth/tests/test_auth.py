ad7834ca29a05a324aa336be73f0b9b578eaf2e8
Dmitriy Boyko <treidman@mail.ru>
2021-02-04 20:30:06 +0500

    add auth test

--- helpers/auth/tests/test_auth.py
+++ helpers/auth/tests/test_auth.py
@@ -10,18 +10,6 @@ def token_data() -> dict:
     return {'id': 1}
 
 
-# @pytest.fixture()
-# def patch_datetime_utcnow(monkeypatch):
-#     fake_utcnow = datetime.datetime.utcnow() - datetime.timedelta(days=7)
-#
-#     class PatchedDatetime:
-#         @classmethod
-#         def utcnow(cls):
-#             return fake_utcnow
-#
-#     monkeypatch.setattr(datetime, 'datetime', PatchedDatetime)
-
-
 def test_read_valid_token(token_data):
 
     request_token = create_token(token_data)
